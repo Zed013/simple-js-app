9debdd0defab2c76e68cc3838c0327c7bccd1918
Zed013 <33998088+Zed013@users.noreply.github.com>
2019-07-01 21:38:59 -0400

    Skeleton of pokedex
    Wll be adding more js soon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
-<script>
-  alert('Hello world');
-</script>
+
+var repository = [
+   [
+   'Bulbasaur', 
+   '2', 
+   'grass, poison'
+   ],
+   [
+   'Ivysaur', 
+   '3', 
+   'grass, poison'
+   ],
+   [
+   'Venusaur',
+   '6', 
+   'grass, poison'
+   ],
+   [
+   'Charmander', 
+   '2', 
+   'fire'
+   ],
+   [
+   'Charmeleon', 
+   '3', 
+   'fire'
+   ],
+   [ 
+   'Charizard', 
+   '5', 
+   'fire, flying'
+   ]
+]   ;
+
+for (var i = 0; i < repository.length; i++) {
+   if (repository[i][1] > 5) {
+     document.write ('<p>Wow! Biggest Pokemon</p>');
+   }
+ 
+   document.write ('<h2>' + repository[i][0] + '</h2>');
+} 
